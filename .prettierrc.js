29c6e95a732382e88558121dec6a7a36553ed042
artvezhl <artvezhl@gmail.com>
2022-07-18 14:41:20 +0300

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    extends: [
+        "stylelint-config-standard-scss",
+        "stylelint-config-prettier-scss"
+    ],
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './App.module.scss'
+
+function App() {
+  const x = 0
+
+  return (
+    <div className={styles.app}>
+      <h1>Cell Game</h1>
+    </div>
+  )
+}
+
+export default App
